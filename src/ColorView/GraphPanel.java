bbebe58f7cf9a9bb292b9283e024a470c21eeebe
ForrestThacker <fyruskasai@gmail.com>
2016-05-03 12:11:19 -0600

    Added GraphPanel

---
+++ src/ColorView/GraphPanel.java
@@ -0,0 +1,33 @@
+package ColorView;
+
+import java.awt.*;
+import javax.swing.JPanel;
+
+/**
+ * 
+ * @author ftha4681
+ *
+ */
+public class GraphPanel  extends JPanel
+{
+	//initialize graphSource here
+	graphSource = new int [] {2, 4, 213, 10, 20, 12, 75};
+	setupPanel();
+	
+}
+
+private void setupPanel()
+{
+	
+}
+
+@Override
+protected void paintComponent(Graphics currentGraphics)
+{
+	super.paintComponent(currentGraphics);
+	
+	Graphics2D mainGraphics = < graphSource.length; index++)
+	{
+		
+	}
+}
